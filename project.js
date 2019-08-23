dbf8d39a02a020803a103e66642cfc70e04370d2
Bojan Krstic <krstic.bojan80@gmail.com>
2019-08-23 14:15:26 +0200

    second commit - projects and about us

---
+++ project.js
@@ -0,0 +1,235 @@
+let displayText = document.querySelector(".about-project");
+
+function displayFirst() {
+  let text = `
+       <h3>One Love Beach Bar Website Design</h3>
+       <h4>This is first project info</h4>
+       <p>Sed ut perspiciatis unde omnis iste natus error 
+       sit voluptatem accusantium doloremque laudantium, 
+       totam rem aperiam, eaque ipsa quae ab illo inventore 
+       veritatis et quasi architecto beatae vitae dicta sunt
+       explicabo. Nemo enim ipsam voluptatem quia voluptas
+       sit aspernatur aut odit aut fugit, sed quia consequuntur
+       magni dolores eos qui ratione voluptatem sequi nesciunt.
+       Neque porro quisquam est, qui dolorem ipsum quia dolor 
+       sit amet, consectetur, adipisci velit, sed quia non 
+       numquam eius modi tempora incidunt ut labore et dolore 
+       magnam aliquam quaerat voluptatem. Ut enim ad minima
+       veniam, quis nostrum exercitationem ullam corporis 
+       suscipit laboriosam, nisi ut aliquid ex ea commodi 
+       consequatur? Quis autem vel eum iure reprehenderit qui in
+       ea voluptate velit esse quam nihil molestiae consequatur,
+       vel illum qui dolorem eum fugiat quo voluptas nulla 
+       pariatur?</p>
+       <a href="https://www.younify.eu" target="_blank" class="btn btn-primary" role="button">Contact Us</a>
+    `;
+  displayText.innerHTML = text;
+}
+
+function displaySecond() {
+  let text = `
+       <h3>Two Love Beach Bar Website Design</h3>
+       <h4>This is second project info</h4>
+       <p>Sed ut perspiciatis unde omnis iste natus error 
+       sit voluptatem accusantium doloremque laudantium, 
+       totam rem aperiam, eaque ipsa quae ab illo inventore 
+       veritatis et quasi architecto beatae vitae dicta sunt
+       explicabo. Nemo enim ipsam voluptatem quia voluptas
+       sit aspernatur aut odit aut fugit, sed quia consequuntur
+       magni dolores eos qui ratione voluptatem sequi nesciunt.
+       Neque porro quisquam est, qui dolorem ipsum quia dolor 
+       sit amet, consectetur, adipisci velit, sed quia non 
+       numquam eius modi tempora incidunt ut labore et dolore 
+       magnam aliquam quaerat voluptatem. Ut enim ad minima
+       veniam, quis nostrum exercitationem ullam corporis 
+       suscipit laboriosam, nisi ut aliquid ex ea commodi 
+       consequatur? Quis autem vel eum iure reprehenderit qui in
+       ea voluptate velit esse quam nihil molestiae consequatur,
+       vel illum qui dolorem eum fugiat quo voluptas nulla 
+       pariatur?</p>
+       <a href="https://www.younify.eu" target="_blank" class="btn btn-primary" role="button">Contact Us</a>
+    `;
+  displayText.innerHTML = text;
+}
+
+function displayThird() {
+  let text = `
+       <h3>Three Love Beach Bar Website Design</h3>
+       <h4>This is third project info</h4>
+       <p>Sed ut perspiciatis unde omnis iste natus error 
+       sit voluptatem accusantium doloremque laudantium, 
+       totam rem aperiam, eaque ipsa quae ab illo inventore 
+       veritatis et quasi architecto beatae vitae dicta sunt
+       explicabo. Nemo enim ipsam voluptatem quia voluptas
+       sit aspernatur aut odit aut fugit, sed quia consequuntur
+       magni dolores eos qui ratione voluptatem sequi nesciunt.
+       Neque porro quisquam est, qui dolorem ipsum quia dolor 
+       sit amet, consectetur, adipisci velit, sed quia non 
+       numquam eius modi tempora incidunt ut labore et dolore 
+       magnam aliquam quaerat voluptatem. Ut enim ad minima
+       veniam, quis nostrum exercitationem ullam corporis 
+       suscipit laboriosam, nisi ut aliquid ex ea commodi 
+       consequatur? Quis autem vel eum iure reprehenderit qui in
+       ea voluptate velit esse quam nihil molestiae consequatur,
+       vel illum qui dolorem eum fugiat quo voluptas nulla 
+       pariatur?</p>
+       <a href="https://www.younify.eu" target="_blank" class="btn btn-primary" role="button">Contact Us</a>
+    `;
+  displayText.innerHTML = text;
+}
+
+function displayFourth() {
+  let text = `
+       <h3>Four Love Beach Bar Website Design</h3>
+       <h4>This is fourth project info</h4>
+       <p>Sed ut perspiciatis unde omnis iste natus error 
+       sit voluptatem accusantium doloremque laudantium, 
+       totam rem aperiam, eaque ipsa quae ab illo inventore 
+       veritatis et quasi architecto beatae vitae dicta sunt
+       explicabo. Nemo enim ipsam voluptatem quia voluptas
+       sit aspernatur aut odit aut fugit, sed quia consequuntur
+       magni dolores eos qui ratione voluptatem sequi nesciunt.
+       Neque porro quisquam est, qui dolorem ipsum quia dolor 
+       sit amet, consectetur, adipisci velit, sed quia non 
+       numquam eius modi tempora incidunt ut labore et dolore 
+       magnam aliquam quaerat voluptatem. Ut enim ad minima
+       veniam, quis nostrum exercitationem ullam corporis 
+       suscipit laboriosam, nisi ut aliquid ex ea commodi 
+       consequatur? Quis autem vel eum iure reprehenderit qui in
+       ea voluptate velit esse quam nihil molestiae consequatur,
+       vel illum qui dolorem eum fugiat quo voluptas nulla 
+       pariatur?</p>
+       <a href="https://www.younify.eu" target="_blank" class="btn btn-primary" role="button">Contact Us</a>
+    `;
+  displayText.innerHTML = text;
+}
+
+function displayFifth() {
+  let text = `
+       <h3>Five Love Beach Bar Website Design</h3>
+       <h4>This is fifth project info</h4>
+       <p>Sed ut perspiciatis unde omnis iste natus error 
+       sit voluptatem accusantium doloremque laudantium, 
+       totam rem aperiam, eaque ipsa quae ab illo inventore 
+       veritatis et quasi architecto beatae vitae dicta sunt
+       explicabo. Nemo enim ipsam voluptatem quia voluptas
+       sit aspernatur aut odit aut fugit, sed quia consequuntur
+       magni dolores eos qui ratione voluptatem sequi nesciunt.
+       Neque porro quisquam est, qui dolorem ipsum quia dolor 
+       sit amet, consectetur, adipisci velit, sed quia non 
+       numquam eius modi tempora incidunt ut labore et dolore 
+       magnam aliquam quaerat voluptatem. Ut enim ad minima
+       veniam, quis nostrum exercitationem ullam corporis 
+       suscipit laboriosam, nisi ut aliquid ex ea commodi 
+       consequatur? Quis autem vel eum iure reprehenderit qui in
+       ea voluptate velit esse quam nihil molestiae consequatur,
+       vel illum qui dolorem eum fugiat quo voluptas nulla 
+       pariatur?</p>
+       <a href="https://www.younify.eu" target="_blank" class="btn btn-primary" role="button">Contact Us</a>
+    `;
+  displayText.innerHTML = text;
+}
+
+function displaySixth() {
+  let text = `
+       <h3>Six Love Beach Bar Website Design</h3>
+       <h4>This is sixth project info</h4>
+       <p>Sed ut perspiciatis unde omnis iste natus error 
+       sit voluptatem accusantium doloremque laudantium, 
+       totam rem aperiam, eaque ipsa quae ab illo inventore 
+       veritatis et quasi architecto beatae vitae dicta sunt
+       explicabo. Nemo enim ipsam voluptatem quia voluptas
+       sit aspernatur aut odit aut fugit, sed quia consequuntur
+       magni dolores eos qui ratione voluptatem sequi nesciunt.
+       Neque porro quisquam est, qui dolorem ipsum quia dolor 
+       sit amet, consectetur, adipisci velit, sed quia non 
+       numquam eius modi tempora incidunt ut labore et dolore 
+       magnam aliquam quaerat voluptatem. Ut enim ad minima
+       veniam, quis nostrum exercitationem ullam corporis 
+       suscipit laboriosam, nisi ut aliquid ex ea commodi 
+       consequatur? Quis autem vel eum iure reprehenderit qui in
+       ea voluptate velit esse quam nihil molestiae consequatur,
+       vel illum qui dolorem eum fugiat quo voluptas nulla 
+       pariatur?</p>
+       <a href="https://www.younify.eu" target="_blank" class="btn btn-primary" role="button">Contact Us</a>
+    `;
+  displayText.innerHTML = text;
+}
+
+function displaySeventh() {
+  let text = `
+       <h3>Seven Love Beach Bar Website Design</h3>
+       <h4>This is seventh project info</h4>
+       <p>Sed ut perspiciatis unde omnis iste natus error 
+       sit voluptatem accusantium doloremque laudantium, 
+       totam rem aperiam, eaque ipsa quae ab illo inventore 
+       veritatis et quasi architecto beatae vitae dicta sunt
+       explicabo. Nemo enim ipsam voluptatem quia voluptas
+       sit aspernatur aut odit aut fugit, sed quia consequuntur
+       magni dolores eos qui ratione voluptatem sequi nesciunt.
+       Neque porro quisquam est, qui dolorem ipsum quia dolor 
+       sit amet, consectetur, adipisci velit, sed quia non 
+       numquam eius modi tempora incidunt ut labore et dolore 
+       magnam aliquam quaerat voluptatem. Ut enim ad minima
+       veniam, quis nostrum exercitationem ullam corporis 
+       suscipit laboriosam, nisi ut aliquid ex ea commodi 
+       consequatur? Quis autem vel eum iure reprehenderit qui in
+       ea voluptate velit esse quam nihil molestiae consequatur,
+       vel illum qui dolorem eum fugiat quo voluptas nulla 
+       pariatur?</p>
+       <a href="https://www.younify.eu" target="_blank" class="btn btn-primary" role="button">Contact Us</a>
+    `;
+  displayText.innerHTML = text;
+}
+
+function displayEight() {
+  let text = `
+       <h3>Eight Love Beach Bar Website Design</h3>
+       <h4>This is eight project info</h4>
+       <p>Sed ut perspiciatis unde omnis iste natus error 
+       sit voluptatem accusantium doloremque laudantium, 
+       totam rem aperiam, eaque ipsa quae ab illo inventore 
+       veritatis et quasi architecto beatae vitae dicta sunt
+       explicabo. Nemo enim ipsam voluptatem quia voluptas
+       sit aspernatur aut odit aut fugit, sed quia consequuntur
+       magni dolores eos qui ratione voluptatem sequi nesciunt.
+       Neque porro quisquam est, qui dolorem ipsum quia dolor 
+       sit amet, consectetur, adipisci velit, sed quia non 
+       numquam eius modi tempora incidunt ut labore et dolore 
+       magnam aliquam quaerat voluptatem. Ut enim ad minima
+       veniam, quis nostrum exercitationem ullam corporis 
+       suscipit laboriosam, nisi ut aliquid ex ea commodi 
+       consequatur? Quis autem vel eum iure reprehenderit qui in
+       ea voluptate velit esse quam nihil molestiae consequatur,
+       vel illum qui dolorem eum fugiat quo voluptas nulla 
+       pariatur?</p>
+       <a href="https://www.younify.eu" target="_blank" class="btn btn-primary" role="button">Contact Us</a>
+    `;
+  displayText.innerHTML = text;
+}
+
+function displayNine() {
+  let text = `
+       <h3>Nine Love Beach Bar Website Design</h3>
+       <h4>This is nine project info</h4>
+       <p>Sed ut perspiciatis unde omnis iste natus error 
+       sit voluptatem accusantium doloremque laudantium, 
+       totam rem aperiam, eaque ipsa quae ab illo inventore 
+       veritatis et quasi architecto beatae vitae dicta sunt
+       explicabo. Nemo enim ipsam voluptatem quia voluptas
+       sit aspernatur aut odit aut fugit, sed quia consequuntur
+       magni dolores eos qui ratione voluptatem sequi nesciunt.
+       Neque porro quisquam est, qui dolorem ipsum quia dolor 
+       sit amet, consectetur, adipisci velit, sed quia non 
+       numquam eius modi tempora incidunt ut labore et dolore 
+       magnam aliquam quaerat voluptatem. Ut enim ad minima
+       veniam, quis nostrum exercitationem ullam corporis 
+       suscipit laboriosam, nisi ut aliquid ex ea commodi 
+       consequatur? Quis autem vel eum iure reprehenderit qui in
+       ea voluptate velit esse quam nihil molestiae consequatur,
+       vel illum qui dolorem eum fugiat quo voluptas nulla 
+       pariatur?</p>
+       <a href="https://www.younify.eu" target="_blank" class="btn btn-primary" role="button">Contact Us</a>
+    `;
+  displayText.innerHTML = text;
+}
