266f90530ddd46d0166cc0d286be9c043d9d55df
Bojan Krstic <krstic.bojan80@gmail.com>
2019-08-23 22:15:51 +0200

    final commit

--- corporation.js
+++ corporation.js
@@ -3,8 +3,29 @@ let uniqModal = document.getElementById("uniqueModal");
 let respModal = document.getElementById("responsiveModal");
 let sModal = document.getElementById("seoModal");
 
+let precise = document.getElementById("preciseText");
+let unique = document.getElementById("uniqueText");
+let responsive = document.getElementById("responsiveText");
+let seo = document.getElementById("seoText");
+
 function developmentModal() {
-    devModal.style.display = "block";
+  devModal.style.display = "block";
+  let text = `
+       <h3>This is precise development text</h3>
+       <br>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Et placeat voluptas asperiores quibusdam iure accusantium 
+          saepe fuga dignissimos illo iste mollitia corporis harum 
+          ullam laborum rerum error, vero, ducimus soluta.
+       </p>
+       <br>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Et placeat voluptas asperiores quibusdam iure accusantium 
+          saepe fuga dignissimos illo iste mollitia corporis harum 
+          ullam laborum rerum error, vero, ducimus soluta.
+       </p>       
+    `;
+  precise.innerHTML = text;
 }
 function devClose() {
   devModal.style.display = "none";
@@ -12,23 +33,71 @@ function devClose() {
 
 function uniqueModal() {
   uniqModal.style.display = "block";
+  let text = `
+  <h3>This is unique design text</h3>
+  <br>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+     Et placeat voluptas asperiores quibusdam iure accusantium 
+     saepe fuga dignissimos illo iste mollitia corporis harum 
+     ullam laborum rerum error, vero, ducimus soluta.
+  </p>
+  <br>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+     Et placeat voluptas asperiores quibusdam iure accusantium 
+     saepe fuga dignissimos illo iste mollitia corporis harum 
+     ullam laborum rerum error, vero, ducimus soluta.
+  </p>       
+`;
+  unique.innerHTML = text;
 }
 function uniqueClose() {
   uniqModal.style.display = "none";
 }
 
 function responsiveModal() {
-    respModal.style.display = "block";
+  respModal.style.display = "block";
+  let text = `
+       <h3>This is responsive layout text</h3>
+       <br>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Et placeat voluptas asperiores quibusdam iure accusantium 
+          saepe fuga dignissimos illo iste mollitia corporis harum 
+          ullam laborum rerum error, vero, ducimus soluta.
+       </p>
+       <br>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Et placeat voluptas asperiores quibusdam iure accusantium 
+          saepe fuga dignissimos illo iste mollitia corporis harum 
+          ullam laborum rerum error, vero, ducimus soluta.
+       </p>       
+    `;
+  responsive.innerHTML = text;
 }
 function respClose() {
-    respModal.style.display = "none";
+  respModal.style.display = "none";
 }
 
 function seoModal() {
-    sModal.style.display = "block";
+  sModal.style.display = "block";
+  let text = `
+       <h3>This is seo friendly text</h3>
+       <br>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Et placeat voluptas asperiores quibusdam iure accusantium 
+          saepe fuga dignissimos illo iste mollitia corporis harum 
+          ullam laborum rerum error, vero, ducimus soluta.
+       </p>
+       <br>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Et placeat voluptas asperiores quibusdam iure accusantium 
+          saepe fuga dignissimos illo iste mollitia corporis harum 
+          ullam laborum rerum error, vero, ducimus soluta.
+       </p>       
+    `;
+  seo.innerHTML = text;
 }
 function seoClose() {
-    sModal.style.display = "none";
+  sModal.style.display = "none";
 }
 
 window.onclick = function(event) {
